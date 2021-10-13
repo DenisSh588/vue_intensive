1de9752a37924c3c3e84c7feee8ebe310a637a40
DenisSh588 <79074712+DenisSh588@users.noreply.github.com>
2021-10-13 11:03:00 +0300

    Add files via upload

--- app/src/main.js
+++ app/src/main.js
@@ -1,11 +1,24 @@
 const $ = require("jquery");
 
-$.get("./api", (data) => {
-    data.forEach((file) =>{
-        $("body").append("<h1>" + file + "</h1>");
-    });
-}, "JSON");
+function getPagesList() {
+    $("h1").remove();
+    $.get("./api", (data) => {
+        data.forEach((file) =>{
+            $("body").append("<h1>" + file + "</h1>");
+        });
+    }, "JSON");
+}
 
-$("button").click() 
+getPagesList();
 
-$.post("./api/createNewHtmlPage.php");
+$("button").click(() => {
+    $.post("./api/createNewHtmlPage.php", {
+        "name": $("input").val()
+    }, (data) => {
+        getPagesList();
+    })
+    .fail(() =>{
+        alert("Такая страница уже существует!");
+    })
+}) 
+
