71ae55409dea0bb5018a041ef5dc9ca7f7a6a36b
DenisSh588 <79074712+DenisSh588@users.noreply.github.com>
2021-10-20 10:02:00 +0300

    Add files via upload

--- app/api/createNewHtmlPage.php
+++ app/api/createNewHtmlPage.php
@@ -1,7 +1,11 @@
 <?php
-$nemFile = "../../" . $_POST["name"] . ".html";
-if(file_exists($nemFile)) {
-    header("HTTP/1.0 400 Bad Request")
+
+$_POST = json_decode(file_get_contents('php://input'), true);
+
+$newFile = "../../" . $_POST["name"] . ".html";
+
+if(file_exists($newFile)) {
+    header("HTTP/1.0 400 Bad Request");
 } else {
-    fopen($nemFile, "w")
+    fopen($newFile, "w");
 }
--- app/src/main.js
+++ app/src/main.js
@@ -1,4 +1,68 @@
-const $ = require("jquery");
+const Editor = require("./editor");
+
+window.editor = new Editor();
+
+window.onload = () => {
+    window.editor.open("index.html");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const Vue = require("vue");
+const axios = require("axios");
+
+new Vue({
+    el: "#app",
+    data: {
+        "pageList": [],
+        "newPageName": ""
+    },
+    methods: {
+        createPage() {
+            axios
+                .post("./api/createNewHtmlPage.php", { "name": this.newPageName})
+                .then(() => this.updatePageList())
+        },
+        updatePageList() {
+            axios
+                .get("./api/")
+                .then((response) => {
+                    this.pageList = response.data
+             })
+        },
+        deletePage(page){
+            axios
+                .post("./api/deletePage.php", { "name": page })
+                .then(() => this.updatePageList())
+        }
+    },
+    created() {
+        this.updatePageList();
+    },
+})*/
+
+/*const $ = require("jquery");
 
 function getPagesList() {
     $("h1").remove();
@@ -20,5 +84,5 @@ $("button").click(() => {
     .fail(() =>{
         alert("Такая страница уже существует!");
     })
-}) 
+}) */
 
